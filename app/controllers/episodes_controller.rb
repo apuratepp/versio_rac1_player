ba453b33beca2048b0f00ce30691b5ea98b426a8
Josep <apuratepp@gmail.com>
2015-08-09 19:28:50 +0200

    Form for date filtering

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,20 @@
 class EpisodesController < ApplicationController
   def index
-    @episodes = VersioRac1::Podcast::Episode.all
+    @episodes = episodes
+  end
+
+  private
+
+  def date_params
+    params[:date]
+  end
+
+  def episodes
+    if date_params
+      date = DateTime.parse(date_params.map {|k,v| v}.join("-"))
+      VersioRac1::Podcast::Episode.find(date.to_time)
+    else
+      VersioRac1::Podcast::Episode.all
+    end
   end
 end
